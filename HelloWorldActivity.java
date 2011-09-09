c97afb9ae638940ab69a8b419f4b948f2abf647b
Aaron.Fredette <afredett@gmail.com>
2011-09-08 21:13:52 -0400

    Hello World

---
+++ HelloWorldActivity.java
@@ -0,0 +1,17 @@
+package assignment1.testing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloWorldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
+    }
+}
